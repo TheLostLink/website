fafec251f8ea4008724571983cc8724fbab83621
abellinii <wadeabel12@hormail.com>
2018-11-21 21:43:29 -0800

    first commit

---
+++ JS/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+
+$("#menu1").click(function(){
+		if($('.nav').css("display") == 'none'){
+			$('.nav').css("display", "block")
+			$(".nav").animate({opacity:'1'},'5000')
+			$("#logo").animate({marginLeft:'2%'},'1000')
+
+		}else{
+			$('.nav').css("display","none")
+			$(".nav").animate({opacity:'0'},'slow')
+			$("#logo").animate({marginLeft:'40%'},'slow')
+
+		}
+
+       })
+                
+           
+      
+
+
+
+
+
+
+
+
+
+
+
+})
