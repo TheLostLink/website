688b67ded26d883fb46f49851f3a50581b77c307
abellinii <wadeabel12@hormail.com>
2019-01-24 19:40:09 -0800

    about to deploy to heroku

--- JS/main.js
+++ JS/main.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 $("#menu1").click(function(){
 		if($('.nav').css("display") == 'none'){
 			$('.nav').css("display", "block")
+			$('#searchInput').css("display", "none")
 			$(".nav").animate({opacity:'1'},'5000')
 			$("#logo").animate({marginLeft:'2%'},'1000')
 
@@ -11,14 +12,33 @@ $("#menu1").click(function(){
 			$('.nav').css("display","none")
 			$(".nav").animate({opacity:'0'},'slow')
 			$("#logo").animate({marginLeft:'43%'},'slow')
+			$('#searchInput').css("display", "none")
 
 		}
 
-       })
+})
+
+
+       $("#search").click(function(){
+		if($('#searchInput').css("display") == 'none'){
+		$('#searchInput').css("display", "block")
+		}else if(!$("#searchInput").val().length == 0){
+         console.log($("#searchInput").val())
+         $('#searchInput').css("display") == 'none'
+
+          //DO SOMETHING WITH SEARCH RESULTS
+
+
+
+
+		}
+		else{
+			$('#searchInput').css("display", "none")
+		}
                 
            
       
-
+})
 
 
 
---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path'); 
+var app = express();
+var ejs = require('ejs');
+
+
+
+//set port
+var port = process.env.PORT || 8080;
+
+
+//app.set('views', path.join(__dirname, 'public'));
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/public"));
+
+
+//routes
+app.get("/", function(req, res){
+	res.render("index");
+})
+
+
+app.listen(port, function(){
+	console.log("app running");
+})
