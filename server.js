58d5be84cc2345e3343bcdf862a5f8e39ea4a0b0
abellinii <wadeabel12@hormail.com>
2019-02-13 18:45:09 -0800

    check

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get("/", function(req, res){
 
 
 app.get("/logi", function(req, res){
- res.send('GET request to the homepage')
+ res.render("index");
 // res.json({path: __dirname + "login/index.html"});
 
 
